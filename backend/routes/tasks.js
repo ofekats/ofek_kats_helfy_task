0ce7c7fd3e1ed304d6d7abffd9c3e470ce2d9624
ofekats <ofekats@gmail.com>
2025-08-13 10:56:37 +0300

    Endpoints  POST /api/tasks complete

--- backend/routes/tasks.js
+++ backend/routes/tasks.js
@@ -15,8 +15,36 @@ data example:
 
 // GET /api/tasks - Get all tasks
 router.get("/", (req, res) => {
-    console.log("send all tasks");
+    console.log("Get all tasks");
     res.json(data.tasks);
+});
+
+// POST /api/tasks - Create a new task
+router.post("/", (req, res) => {
+    console.log("Create a new task");
+    const { title, description, completed, priority, createdAt } = req.body;
+    
+    //validate data
+    if (!title || typeof title !== "string") {
+        return res.status(400).json({ error: "Title is required and must be a string" });
+    }
+    const validPriorities = ["low", "medium", "high"];
+    if (priority && !validPriorities.includes(priority)) {
+        return res.status(400).json({ error: "Priority must be 'low', 'medium' or 'high'" });
+    }
+
+    const newTask = {
+        id: data.nextId++,
+        title,
+        description: description || "",
+        completed: completed || false,
+        createdAt: createdAt ? new Date(createdAt) : new Date(),
+        priority: priority || "low"
+    };
+
+
+    data.tasks.push(newTask);
+    res.status(201).json(newTask);
 });
 
 
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,10 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 
+//middleware
+app.use(cors());
+app.use(express.json());
 
 app.use("/api/tasks", require("./routes/tasks"));
 
